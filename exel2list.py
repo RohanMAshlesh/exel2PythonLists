2bb55dbc98066f7fe00fe5444223076f4f36f79f
RohanMAshlesh <48551351+RohanMAshlesh@users.noreply.github.com>
2019-08-19 17:02:07 +0530

    Update exel2list.py

--- exel2list.py
+++ exel2list.py
@@ -3,7 +3,7 @@ from xlrd import open_workbook
 book = xlrd.open_workbook("##File_path") #use the  of excel file in actual code.
 sheet = book.sheet_by_index(0)
 
-list_r = []
+list_r = [] #list where excel tupples are stored
 
 for i in range(1,sheet.nrows):
     list_r.append(str(sheet.row_values(i)[r-1]))
